d7dfe706d9fe313ec5ba797557b6eb02d5e780eb
h. mikail <>
2023-07-24 04:40:45 -0700

    rock...''

--- challenge.js
+++ challenge.js
@@ -1,2 +1,18 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+function playGame(player1, player2) {
+    if (player1 === player2) {
+      return "tie"
+    } else if (
+      (player1 === "rock" && player2 === "scissors") 
+      (player1 === "scissors" && player2 === "paper") 
+      (player1 === "paper" && player2 === "rock")
+    ) {
+      return "player1 wins"
+    } else {
+      return "player2 wins"
+    }
+  }
+  
+  
+  console.log(playGame("rock", "paper"))
+  console.log(playGame("scissors", "scissors"))
+  console.log(playGame("paper", "rock"))
